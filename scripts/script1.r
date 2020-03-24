50b6461e26bc914bcb568bfcea5e28f2efff3dbb
Joanna Ulman <joasia.ulman@gmail.com>
2020-03-24 12:55:09 +0100

    JU 2020-03-24

--- scripts/script1.r
+++ scripts/script1.r
@@ -1,15 +1,17 @@
 #załadowanie bibliotek
 library(tm)
+library(hunspell)
+library(stringr)
 
 #zmiana katalogu roboczego
-workDir <- "D:\\JU"
+workDir <- "D:/Studia/Przetwarzanie jezyka/NLP"
 setwd(workDir)
 
 #definicja katalogów funkcjonalnych
-inputDir <- ".\\data"
-outputDir <- ".\\results"
-scriptsDir <- ".\\scripts"
-workspacesDir <- ".\\workspaces"
+inputDir <- "./data"
+outputDir <- "./results"
+scriptsDir <- "./scripts"
+workspacesDir <- "./workspaces"
 dir.create(outputDir, showWarnings = TRUE)
 dir.create(workspacesDir, showWarnings = TRUE)
 
@@ -17,7 +19,7 @@ dir.create(workspacesDir, showWarnings = TRUE)
 corpusDir <-  paste(
   inputDir,
   "Literatura - streszczenia - oryginał",
-  sep = "\\"
+  sep = "/"
 )
 corpus <- VCorpus(
   DirSource(
@@ -37,11 +39,49 @@ corpus <- tm_map(corpus, content_transformer(tolower))
 stoplistFile <- paste(
   inputDir,
   "stopwords_pl.txt",
-  sep = "\\"
+  sep = "/"
 )
 stoplist <- readLines(stoplistFile, encoding = "UTF-8")
 corpus <- tm_map(corpus, removeWords, stoplist)
 corpus <- tm_map(corpus, stripWhitespace)
+
+#usunięcie em dash i 3/4
+remove_char <- content_transformer(function(x, pattern) gsub(pattern, "", x))
+corpus <- tm_map(corpus, removeChar, intToUtf8(8722))
+corpus <- tm_map(corpus, removeChar, intToUtf8(190))
+
+polish <- dictionary(lang="pl_PL")
+
+lemmatize <- function(text) {
+  simpleText <- str_trim(as.character(text))
+  parsedText <- strsplit(simple_text, split = " ")
+  newTextVec <- hunspell_stem(parsedText[[1]], dict = polish)
+  for (i in 1:length(new_text_vec)) {
+    if (length(newTextVec[[i]]) == 0) newTextVec[i] <- parsedText[[1]][i]
+    if (length(newTextVec[[i]]) > 1) newTextVec[i] <- newTextVec[[i]][1]
+  }
+  new_text <- paste(newTextVec, collapse = " ")
+  return(newText)
+}
+
+corpus <- tm_map(corpus, content_transformer(lemmatize))
+
+#usunięcie rozszerzeń z nazw plików
+cutExtensions <- function(document){
+  meta(document, "id") <- gsub(pattern = "/.txt$", replacement = "", meta(document, "id"))
+  return(document)
+}
+
+corpus <- tm_map(corpus, cutExtensions)
+
+#eksport zawartości korpusu do plików tekstowych
+preprocessedDir <- paste(
+  inputDir,
+  "Literatura - streszczenia - przetworzone",
+  sep = "/"
+)
+dir.create(preprocessedDir)
+writeCorpus(corpus, path = preprocessedDir)
 
 #wyświetlenie zawartości pojedynczego dokumentu
 writeLines(as.character(corpus[[1]]))
